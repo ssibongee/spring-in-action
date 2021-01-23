6aee12ed512d77c87b5d4d1b97c0b188f6a9892f
ssibongee <ssibongee@gmail.com>
2021-01-23 12:55:34 +0900

    [#3] 사용자 주문 요청을 처리하는 메서드 구현

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Order;
 
 @Slf4j
 @Controller
@@ -15,5 +17,11 @@ public class OrderController {
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 }
