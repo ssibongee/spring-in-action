7b1e5f668ab568a4f1f60f664f162fa1f37a451e
ssibongee <ssibongee@gmail.com>
2021-01-23 13:09:07 +0900

    [#3] 주문과 재료 선택에 대한 유효성 검사 로직 추가

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,12 +3,14 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -48,7 +50,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
         log.info("Processing design: " + design);
 
         return "redirect:/orders/current";
